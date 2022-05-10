fafa6cb575a2295fbced0055385a1146b0fa5cd0
yasserqamhiah <yaserqamhiah@gmail.com>
2022-05-10 12:21:31 +0300

    First Commit

---
+++ src/main/java/com/example/EEmployee/EEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.EEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EEmployee/EEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
